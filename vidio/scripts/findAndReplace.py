d88824c32696246e49ca7ebcc877223639db57e3
Daniel Thurau <Daniel.n.thurau@gmail.com>
2016-11-29 12:46:29 -0800

    Made.It.Beautiful

--- vidio/scripts/findAndReplace.py
+++ vidio/scripts/findAndReplace.py
@@ -6,18 +6,22 @@ import re
 filein = sys.argv[1]
 fileout = sys.argv[2]
 textToReplace1 = sys.argv[3]
-
+textToReplace2 = sys.argv[4]
 
 
 #-----------------------------------------------------------
-textToSearch1 = "!!<client:/server/share>!!"
+textToSearch1 = "!!<Working Dir>!!"
 #textToReplace1 = r"whatever ot os"
+textToSearch2 = "!!<Target Dir>!!"
+#textToReplace2 = r"dtg/adminsitrator"
+
 
 f = open(filein,'r')
 filedata = f.read()
 f.close()
 
 filedata = filedata.replace(textToSearch1,textToReplace1)
+filedata = filedata.replace(textToSearch2,textToReplace2)
 f = open(fileout,'w')
 f.write(filedata)
 f.close()
